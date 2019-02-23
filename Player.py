84ed33509300e63be3c8a0b5be1077b050ceeaba
Raxkas <rxrhhx@gmail.com>
2019-02-24 01:49:14 +0300

    Replace some unnamed nums with FACE_SIZE

--- Player.py
+++ Player.py
@@ -18,17 +18,19 @@ class Player(object):
     def draw(self):
         """ Draw player. """
         # Draw circle
-        height = 260 / (Game.N * 3)
-        width = 300 / (Game.N * 3)
-        arcade.draw_circle_filled(round(width * (self.x / 2)),
-                                  round(height * ((0.33 + 0.33 * (self.x % 2 == self.y % 2)) + self.y)),
+        edge_size = 3*Game.N
+        face_width, face_height = Game.FACE_SIZE
+        cell_width = face_width / edge_size
+        cell_height = face_height / edge_size
+        arcade.draw_circle_filled(round(cell_width * (self.x / 2)),
+                                  round(cell_height * ((0.33 + 0.33 * (self.x % 2 == self.y % 2)) + self.y)),
                                   3,
                                   arcade.color.BLACK)
         if self.display_coordinates:
             # Draw numbers
             arcade.draw_text(str(self.x) + ' ' + str(self.y),
-                             round(width * (self.x / 2)),
-                             round(height * (0.33 * (self.x % 2 == self.y % 2) + self.y) + 30),
+                             round(cell_width * (self.x / 2)),
+                             round(cell_height * (0.33 * (self.x % 2 == self.y % 2) + self.y) + 30),
                              arcade.color.BLACK,
                              17,
                              bold=True,
